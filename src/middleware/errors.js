1b2978b63a90b84962652959aee6e9584f0a722b
袁首京 <yuanshoujing@gmail.com>
2024-05-10 11:42:43 +0800

    0.1.5

---
+++ src/middleware/errors.js
@@ -0,0 +1,96 @@
+export class NBError extends Error {
+  constructor(message, code = 500) {
+    super(message);
+    this.code = code;
+  }
+}
+
+export class BadRequest extends NBError {
+  constructor(message = "Bad Request") {
+    super(message, 400);
+  }
+}
+
+export class Unauthorized extends NBError {
+  constructor(message = "Unauthorized") {
+    super(message, 401);
+  }
+}
+
+export class Forbidden extends NBError {
+  constructor(message = "Forbidden") {
+    super(message, 403);
+  }
+}
+
+export class NotFound extends NBError {
+  constructor(message = "Not Found") {
+    super(message, 404);
+  }
+}
+
+export class MethodNotAllowed extends NBError {
+  constructor(message = "Method Not Allowed") {
+    super(message, 405);
+  }
+}
+
+export class NotAcceptable extends NBError {
+  constructor(message = "Not Acceptable") {
+    super(message, 406);
+  }
+}
+
+export class ProxyAuthenticationRequired extends NBError {
+  constructor(message = "Proxy Authentication Required") {
+    super(message, 407);
+  }
+}
+
+export class Timeout extends NBError {
+  constructor(message = "Time-out") {
+    super(message, 408);
+  }
+}
+
+export class Conflict extends NBError {
+  constructor(message = "Conflict") {
+    super(message, 409);
+  }
+}
+
+export class InternalServerError extends NBError {
+  constructor(message = "Internal Server Error") {
+    super(message, 500);
+  }
+}
+
+export class NotImplemented extends NBError {
+  constructor(message = "Not Implemented") {
+    super(message, 501);
+  }
+}
+
+export class BadGateway extends NBError {
+  constructor(message = "Bad Gateway") {
+    super(message, 502);
+  }
+}
+
+export class ServiceUnavailable extends NBError {
+  constructor(message = "Service Unavailable") {
+    super(message, 503);
+  }
+}
+
+export function errorHandle() {
+  return async function (ctx, next) {
+    try {
+      await next();
+    } catch (e) {
+      if (e instanceof NBError) {
+        ctx.throw(e.code, e.message);
+      }
+    }
+  };
+}
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,4 +1,5 @@
 export { dispatcher } from "./dispatcher";
 export { authentication } from "./auth";
 export { rjson } from "./json";
+export * from "./errors";
 export * from "./token";
